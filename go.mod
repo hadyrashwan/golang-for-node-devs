1c71fd1ca4fe30a04d6d4ce8589b581fb8e7216a
Hady Rashwan <h2rashwan@gmail.com>
2024-08-05 16:39:44 +0100

    move to sqlite ( turso)

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.5
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/gofiber/fiber/v2 v2.52.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -12,8 +13,11 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240723183952-b944339d7e70 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"reflect"
+
 	"github.com/gofiber/fiber/v2"
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type Todo struct {
@@ -14,26 +18,40 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
+type Exec struct {
+	LastInsertId int64 `json:"lastInsertId"`
+	RowsAffected int64 `json:"rowsAffected"`
+}
 
 func main() {
-	
 
 	err := godotenv.Load()
 	if err != nil {
-	  log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file")
 	}
 
 	PORT := os.Getenv("PORT")
+	DB_URL := os.Getenv("DB_URL")
+	DB_TOKEN := os.Getenv("DB_TOKEN")
 
+	url := fmt.Sprintf("%s?authToken=%s", DB_URL, DB_TOKEN)
+
+	db, err := sql.Open("libsql", url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s: %s", url, err)
+		os.Exit(1)
+	}
 
 	app := fiber.New()
 
 	// create todos example
 	todos := []Todo{}
 
-
 	app.Get("/api/todos/", func(c *fiber.Ctx) error {
-		// return c.SendString("Hello, World!")
+		todos, err = db_query_helper[Todo](db, "SELECT * FROM todos")
+		if err != nil {
+			return err
+		}
 		return c.Status(200).JSON(fiber.Map{
 			"todos": todos,
 		})
@@ -42,51 +60,132 @@ func main() {
 	// create a post endpoint for todos
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{}
-
 		if err := c.BodyParser(todo); err != nil {
 			return err
 		}
-
 		if todo.Body == "" {
 			return c.Status(400).JSON(fiber.Map{
 				"error": "Todo body cannot be empty",
 			})
 		}
-		todo.ID = len(todos) + 1
-		todos = append(todos, *todo)
+		query := "INSERT INTO todos (body, completed) VALUES (?, ?)"
+
+		result, err := db_exec_helper[Todo](db, query, todo.Body, todo.Completed)
+		if err != nil {
+			return err
+		}
+		todo.ID = int(result.LastInsertId)
 		return c.Status(201).JSON(todo)
 	})
 
 	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
 		id := c.Params("id")
-		// todo := Todo{}
-		// loop over todos and find hte matching by id
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i])
-			}
+
+		query := `
+		UPDATE todos
+		SET completed = ?
+		WHERE id = ?`
+		result, err := db_exec_helper[Todo](db, query,true, id)
+		if err != nil {
+			return err
 		}
-		return c.Status(400).JSON(fiber.Map{
-			"error": "Todo not found",
-		})
+		if result.RowsAffected == 0{
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Todo not found",
+			})
+		}
+		todos, err = db_query_helper[Todo](db, "SELECT * FROM todos WHERE id = ?", id)
+		if err != nil {
+			return err
+		}
+		return c.Status(200).JSON(todos[0])
+		
 	})
 
 	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
 		id := c.Params("id")
-		for i, todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				// delete matched item from array
-				todos = append(todos[:i], todos[i+1:]...)
-				return c.Status(200).JSON(fiber.Map{
-					"message": "Todo deleted",
-				})
-			}
-			}
-		return c.Status(400).JSON(fiber.Map{
-			"error": "Todo not found",
+
+		query := `
+		DELETE FROM todos WHERE id = ?`
+		result, err := db_exec_helper[Todo](db, query, id)
+		if err != nil {
+			return err
+		}
+		if result.RowsAffected == 0 {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Todo not found",
+			})
+		}
+		return c.Status(200).JSON(fiber.Map{
+			"message": "Todo deleted",
 		})
+
 	})
 
 	log.Fatal(app.Listen(":" + PORT))
+}
+
+func db_query_helper[T any](db *sql.DB, query string, args ...interface{}) ([]T, error) {
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	// rows.Close()
+
+	var objects []T
+
+	for rows.Next() {
+		var object T
+
+		s := reflect.ValueOf(&object).Elem()
+		numCols := s.NumField()
+		columns := make([]interface{}, numCols)
+		for i := 0; i < numCols; i++ {
+			field := s.Field(i)
+			columns[i] = field.Addr().Interface()
+		}
+
+		err := rows.Scan(columns...)
+		if err != nil {
+			fmt.Println("Error scanning row:", err)
+			return nil, err
+		}
+
+		objects = append(objects, object)
+		// fmt.Println(user.ID, user.Name)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+	return objects, nil
+}
+
+func db_exec_helper[T any](db *sql.DB, query string, args ...interface{}) (Exec, error) {
+	exec, err := db.Exec(query, args...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	// rows.Close()
+
+	id, err := exec.LastInsertId()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	rows, err := exec.RowsAffected()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	result := Exec{
+		LastInsertId: id,
+		RowsAffected: rows,
+	}
+	exec.LastInsertId()
+	exec.RowsAffected()
+
+	return result, nil
 }
