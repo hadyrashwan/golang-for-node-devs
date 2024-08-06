62ac7f5254799345bef2895cb526e7256a2e2ffd
Hady Rashwan <h2rashwan@gmail.com>
2024-08-06 12:29:38 +0100

    Add nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Container, Stack } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Stack h='100vh'>
+     <Navbar>
+
+    <Container> 
+      {/* TodoForm */}
+      {/* TodoList */}
+    </Container>
+     </Navbar>
+    <Button onClick={() => setCount( (count) => count + 1)} > Hello  count is {count} </Button>
+    </Stack>
   )
 }
 
